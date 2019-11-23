c26900b39e8ea612ecd4d0667d187735e175f904
kmanikandan <kmanikandangce@gmail.com>
2019-11-23 15:30:24 +0530

    o

--- react_redux/src/index.js
+++ react_redux/src/index.js
@@ -8,9 +8,7 @@ import {Provider} from 'react-redux'
 import { combineReducers, createStore } from 'redux';
 import userReducer from './user-reducer';
 import productReducer from './product-reducer';
-
-
-
+import {UPDATE_USER} from './user-action'
 const allReducer = combineReducers({
   products: productReducer,
   user: userReducer
@@ -26,16 +24,16 @@ const store = createStore(allReducer, {
 console.log(store.getState());
 
 const updateUserAction = {
-  type: 'updateUser',
+  type: UPDATE_USER,
   payload: {
-    user: 'gwtham'
+    user: 'gowtham'
   }
 
 }
 
 store.dispatch(updateUserAction);
 
-console.log(store.getState());
+console.log("after update",store.getState());
 
 
 ReactDOM.render(<Provider store={store}><App context="this is context" /></Provider>, document.getElementById('root'));
